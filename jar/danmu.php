9c390a92cc021b4a35a033e95bab5a093d04dd6c
aliluya1977 <150768790+aliluya1977@users.noreply.github.com>
2024-06-01 15:29:21 +0800

    Add files via upload

---
+++ jar/danmu.php
@@ -0,0 +1,402 @@
+<!doctype html>
+<html lang="en">
+ <head>
+  <meta name="360-analysis-id" content="1k1c1k1e39331c2v">
+  <meta charset="utf-8">
+  <!--!‫‭‬‫‬‪‫‭‬‫‬‬‭‫‭‭‪‭‫‬‪‬‏‫--> 
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge"> 
+  <meta name="viewport" id="viewport" content="width=device-width, initial-scale=1,user-scalable=no"> 
+  <link rel="shortcut icon" type="image/x-icon" href="css/favicon.ico"> 
+  <title>摸鱼4K | 导航</title> 
+  <meta name="description" content="摸鱼儿的个人网站，期待与优秀的你相遇！"> 
+  <meta name="keywords" content="摸鱼儿"> 
+  <link rel="stylesheet" type="text/css" href="css/h.9c69ed6c.css"> 
+  <link rel="stylesheet" type="text/css" href="css/nekotora.99cf6f8c.css"> 
+  <script async src="js/js"></script>
+  <!--!‫‭‬‫‬‪‫‭‬‫‬‬‭‫‭‭‪‭‫‬‪‬‏‫--> 
+  <script>function a() { dataLayer.push(arguments) } window.dataLayer = window.dataLayer || [], a("js", new Date), a("config", "UA-137340638-1");</script>  
+  <meta charset="utf-8&quot;/"> 
+  <style>
+          mark {
+              background-color:#CCCCCC; font-weight:bold;
+          }
+      </style> 
+  <style>
+    .toast-bottom-center {
+        position: absolute;
+        left: 30px;
+        right: 10px;
+        top: 20px;
+    }
+</style>
+  <!--!‫‭‬‫‬‪‫‭‬‫‬‬‭‫‭‭‪‭‫‬‪‬‏‫-->
+  <script async src="js/null_9012335efd8741af8aa90f736cd38571.js"></script>
+  <script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-3NF90DWZG8');
+</script>
+ </head> 
+ <!-- Google tag (gtag.js) -->   
+ <body>
+  <div class="background"> 
+   <canvas id="startrack"></canvas> 
+   <div class="cover"></div> 
+  </div> 
+  <div class="main"> 
+   <div class="ch intro"> 
+    <div class="container"> 
+     <div class="hello"> 
+      <h1 id="slogan">Hi~</h1> 
+      <!--随机文字信息在\js\page.3a0791a3.js中修改--> 
+      <h2> 
+       <div class="circle"> 
+        <span></span> 
+        <span></span> 
+        <span></span> 
+       </div> 摸鱼 </h2> 
+     </div> 
+    </div> 
+   </div> 
+   <div class="find ch"> 
+    <div class="container links"> 
+     <h2 class="chtitle">TVbox <span>接口（点击复制）</span></h2> 
+     <div class="clear col-3"> 
+      <div class="item"> 
+       <div class="bg" style="background-color:#28a9e0"></div> 
+       <div class="inner copy-btn" id="yunxing" data-clipboard-text="http://我不是.摸鱼儿.top"> 
+        <span>摸鱼4K</span> 
+       </div> 
+      </div> 
+            <div class="item"> 
+       <div class="bg" style="background-color:#f30b0b"></div> 
+       <div class="inner copy-btn" id="yunxing" data-clipboard-text="http://小鱼干.摸鱼儿.top"> 
+        <span>摸鱼多线</span> 
+       </div> 
+      </div>
+      <div class="item"> 
+       <div class="bg" style="background-color:#28a9e0"></div> 
+       <div class="inner copy-btn" id="yunxing" data-clipboard-text="http://清清.摸鱼儿.top"> 
+        <span>清清摸鱼</span> 
+       </div> 
+      </div> 
+      <div class="item"> 
+       <div class="bg" style="background-color:#c4e00f"></div> 
+       <div class="inner copy-btn" id="yunxing" data-clipboard-text="http://饭太硬.top/tv"> 
+        <span>饭太硬</span> 
+       </div> 
+      </div> 
+      <div class="item"> 
+       <div class="bg" style="background-color:#F7DC6F"></div> 
+       <div class="inner copy-btn" id="yunxing" data-clipboard-text="http://xhww.fun:63/小米/DEMO.json"> 
+        <span>小米 小暴脾气</span> 
+       </div> 
+      </div> 
+      <div class="item"> 
+       <div class="bg" style="background-color:#c4e00f"></div> 
+       <div class="inner copy-btn" id="junyu" data-clipboard-text="https://fm.t4tv.hz.cz/json/config.json"> 
+        <span>FongMI</span> 
+       </div> 
+      </div> 
+      <div class="item"> 
+       <div class="bg" style="background-color:#5499C7"></div> 
+       <div class="inner copy-btn" id="xiaoya" data-clipboard-text="https://100km.top/0"> 
+        <span>纯一骚零</span> 
+       </div> 
+      </div> 
+      <div class="item"> 
+       <div class="bg" style="background-color:#1ba050"></div> 
+       <div class="inner copy-btn" id="tangsan" data-clipboard-text="https://jihulab.com/okcaptain/kko/-/raw/main/ok.txt"> 
+        <span>OK屌炸天 </span> 
+       </div> 
+      </div> 
+      <div class="item"> 
+       <div class="bg" style="background-color:#1f3cfb"></div> 
+       <div class="inner copy-btn" id="qiaoer" data-clipboard-text="http://cdn.qiaoji8.com/tvbox.json"> 
+        <span>巧技</span> 
+       </div> 
+      </div> 
+      <div class="item"> 
+       <div class="bg" style="background-color:#d50cf0"></div> 
+       <div class="inner copy-btn" id="yunxing" data-clipboard-text="https://api-lmteam.koyeb.app/files/py.json"> 
+        <span>霜辉月明 py</span> 
+       </div> 
+      </div> 
+      <div class="item"> 
+       <div class="bg" style="background-color:#0ff9ca"></div> 
+       <div class="inner copy-btn" id="cainisi" data-clipboard-text="https://tvbox.cainisi.cf"> 
+        <span>菜妮丝 XBPQ</span> 
+       </div> 
+      </div> 
+      <div class="item"> 
+       <div class="bg" style="background-color:#2ECC71"></div> 
+       <div class="inner copy-btn" id="xiaoya" data-clipboard-text="https://mirror.ghproxy.com/raw.githubusercontent.com/gaotianliuyun/gao/master/0825.json"> 
+        <span>PG one</span> 
+       </div> 
+      </div> 
+      <div class="item"> 
+       <div class="bg" style="background-color:#f30b0b"></div> 
+       <div class="inner copy-btn" id="shenqi" data-clipboard-text="https://github.moeyy.xyz/https://raw.githubusercontent.com/xyq254245/xyqonlinerule/main/XYQTVBox.json"> 
+        <span>香雅情 XYQ</span> 
+       </div> 
+      </div> 
+      <div class="item"> 
+       <div class="bg" style="background-color:#E74C3C"></div> 
+       <div class="inner copy-btn" id="xiaoya" data-clipboard-text="http://肥猫.live"> 
+        <span>肥猫</span> 
+       </div> 
+      </div> 
+      <div class="item"> 
+       <div class="bg" style="background-color:#2ECC71"></div> 
+       <div class="inner copy-btn" id="yunxing" data-clipboard-text="http://www.wya6.cn/tv/yc.php"> 
+        <span>无意</span> 
+       </div> 
+      </div> 
+      <div class="item"> 
+       <div class="bg" style="background-color:#5d4106"></div> 
+       <div class="inner copy-btn" id="xiaoya" data-clipboard-text="https://weixine.net/ysc.json"> 
+        <span>运输车</span> 
+       </div> 
+      </div> 
+      <div class="item"> 
+       <div class="bg" style="background-color:#2ECC71"></div> 
+       <div class="inner copy-btn" id="xiaoya" data-clipboard-text="https://yydsys.top/duo"> 
+        <span>多多</span> 
+       </div> 
+      </div> 
+     </div> 
+    </div> 
+   </div> 
+   <div class="gate ch"> 
+    <div class="container links"> 
+     <h2 class="chtitle">相关推荐<span>摸鱼4K</span></h2> 
+     <div class="clear col-3"> 
+      <a href="https://wb.xiyan.life/#/pages/forum/info?id=204" target="_blank"> 
+       <div class="item"> 
+        <div class="avatar"> 
+         <img src="https://www.freeimg.cn/i/2024/01/09/659c4839316ba.jpg"> 
+        </div> 
+        <div class="inner"> 
+         <h5>摸鱼4K全家桶</h5> 
+         <p>摸鱼4K全系列资源合集</p> 
+        </div> 
+       </div> </a>
+      <a href="http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=G_OftHdSmJkvy8tAikXcfMj_qUCQQ_9t&authKey=%2BSOQYQimOHmFVmtROyHQiiRkkft1UfnYujgWPMxpHzrkJ51M2HulXCp8zetWPP50&noverify=0&group_code=188368726" target="_blank"> 
+       <div class="item"> 
+        <div class="avatar"> 
+         <img src="https://www.freeimg.cn/i/2024/01/09/659c48b790f70.jpg"> 
+        </div> 
+        <div class="inner"> 
+         <h5>摸鱼交流群</h5> 
+         <p>讨论反馈的小窝</p> 
+        </div> 
+       </div> </a> 
+      <a href="https://pd.qq.com/s/hl47rvyfs" target="_blank"> 
+       <div class="item"> 
+        <div class="avatar"> 
+         <img src="https://img1.baidu.com/it/u=4192535236,365996889&fm=253&fmt=auto?w=353&h=235"> 
+        </div> 
+        <div class="inner"> 
+         <h5>QQ频道</h5> 
+         <p>资源反馈频道</p> 
+        </div> 
+       </div> </a>
+      <a href="https://wb.xiyan.life/" target="_blank"> 
+       <div class="item"> 
+        <div class="avatar"> 
+         <img src="https://s11.ax1x.com/2023/11/28/piBbNKP.jpg"> 
+        </div> 
+        <div class="inner"> 
+         <h5>夕颜社区</h5> 
+         <p>软件交流社区资讯</p> 
+        </div> 
+       </div> </a> 
+      <a href="https://link3.cc/shimiandeye" target="_blank"> 
+       <div class="item"> 
+        <div class="avatar"> 
+         <img src="https://www.freeimg.cn/i/2024/04/06/66114c455c07d.webp"> 
+        </div> 
+        <div class="inner"> 
+         <h5>TvBox精品</h5> 
+         <p>by失眠的夜</p> 
+        </div> 
+       </div> </a>
+      <a href="http://qiqiv.cn/tvbox" target="_blank"> 
+       <div class="item"> 
+        <div class="avatar"> 
+         <img src="https://s11.ax1x.com/2024/01/05/pixnPTU.png"> 
+        </div> 
+        <div class="inner"> 
+         <h5>tvbox配置编辑</h5> 
+         <p>配置编辑器，支持加解密</p> 
+        </div> 
+       </div> </a>  
+      <a href="https://t.me/bh_tvbox" target="_blank"> 
+       <div class="item"> 
+        <div class="avatar"> 
+         <img src="https://s11.ax1x.com/2024/01/05/pixnZlR.jpg"> 
+        </div> 
+        <div class="inner"> 
+         <h5>宝盒 发布频道</h5> 
+         <p>Tvbox源码二开，美化UI，自动换源，支持多订阅</p> 
+        </div> 
+       </div> </a> 
+       <div class="item"> 
+        <div class="avatar"> 
+         <img src="https://s11.ax1x.com/2024/01/05/pixne61.jpg"> 
+        </div> 
+        <div class="inner"> 
+         <h5>影迷 发布频道</h5> 
+         <p>Tvbox源码二开，手机竖屏版，UI新颖，功能不断进化中</p> 
+        </div> 
+       </div> </a>
+      <a href="https://github.com/q215613905/TVBoxOS" target="_blank"> 
+       <div class="item"> 
+        <div class="avatar"> 
+         <img src="https://s11.ax1x.com/2024/01/05/pixnkY4.png"> 
+        </div> 
+        <div class="inner"> 
+         <h5>俊于仓库</h5> 
+         <p>Tvbox源码加更，延续官仓风格的版本</p> 
+        </div> 
+       </div> </a> 
+      <a href="https://github.com/takagen99/Box" target="_blank"> 
+       <div class="item"> 
+        <div class="avatar"> 
+         <img src="https://s11.ax1x.com/2024/01/05/pixnum6.png"> 
+        </div> 
+        <div class="inner"> 
+         <h5>takagen99仓库</h5> 
+         <p>基于Tvbox官仓源码，修改美化UI</p> 
+        </div> 
+       </div> </a> 
+      <a href="https://github.com/FongMi/TV" target="_blank"> 
+       <div class="item"> 
+        <div class="avatar"> 
+         <img src="https://s11.ax1x.com/2024/01/05/pixnK0K.png"> 
+        </div> 
+        <div class="inner"> 
+         <h5>FongMI仓库</h5> 
+         <p>台湾活雷锋，极简而强大的播放器</p> 
+        </div> 
+       </div> </a> 
+      <a href="https://www.foodieguide.com/iptvsearch/?s=" target="_blank"> 
+       <div class="item"> 
+        <div class="avatar"> 
+         <img src="https://s11.ax1x.com/2024/01/05/pixnMTO.png"> 
+        </div> 
+        <div class="inner"> 
+         <h5>直播源搜索引擎</h5> 
+         <p>国内外电视直播源搜索，m3u8、flv、rtsp……</p> 
+        </div> 
+       </div> </a> 
+      <a href="https://guihet.com/tvlistconvert.html#zbyjg" target="_blank"> 
+       <div class="item"> 
+        <div class="avatar"> 
+         <img src="/zbzh.webp"> 
+        </div> 
+        <div class="inner"> 
+         <h5>直播源格式转换</h5> 
+         <p>直播源格式在线转换工具</p> 
+        </div> 
+       </div> </a> 
+      <a href="https://github.com/hjdhnx/dr_py" target="_blank"> 
+       <div class="item"> 
+        <div class="avatar"> 
+         <img src="https://www.freeimg.cn/i/2024/01/07/6599ba9a6c1be.png"> 
+        </div> 
+        <div class="inner"> 
+         <h5>道长DRPY项目</h5> 
+         <p>可直接用于tvbox和pluto软件数据源使用</p> 
+        </div> 
+       </div> </a> 
+      <a href="https://c.runoob.com" target="_blank"> 
+       <div class="item"> 
+        <div class="avatar"> 
+         <img src="https://www.freeimg.cn/i/2024/01/07/6599bd366cc3e.jpeg"> 
+        </div> 
+        <div class="inner"> 
+         <h5>菜鸟工具</h5> 
+         <p>常用工具大全,包括js美化base64加解密</p> 
+        </div> 
+       </div> </a> 
+      <a href="https://jihulab.com" target="_blank"> 
+       <div class="item"> 
+        <div class="avatar"> 
+         <img src="https://www.freeimg.cn/i/2024/01/07/6599bcac4ea21.jpeg"> 
+        </div> 
+        <div class="inner"> 
+         <h5>极狐代码托管</h5> 
+         <p>稳定速度快，个人私用接口托管足够了~</p> 
+        </div> 
+       </div> </a> 
+      <a href="https://bar.ssstab.com/" target="_blank"> 
+       <div class="item"> 
+        <div class="avatar"> 
+         <img src="https://www.freeimg.cn/i/2024/01/07/6599bb74d44c3.jpeg"> 
+        </div> 
+        <div class="inner"> 
+         <h5>sssbar搜索</h5> 
+         <p>工具箱搜索网站，非常的方便且强大</p> 
+        </div> 
+       </div> </a> 
+      <a href="https://adzhp.net/?ref=www.widiz.com" target="_blank"> 
+       <div class="item"> 
+        <div class="avatar"> 
+         <img src="https://www.freeimg.cn/i/2024/01/07/6599ba9a5a348.png"> 
+        </div> 
+        <div class="inner"> 
+         <h5>爱达杂货铺</h5> 
+         <p>收集那些有用的东西</p> 
+        </div> 
+       </div> </a> 
+     </div> 
+     <!--底部波浪--> 
+     <div id="hans-bolang"></div>
+     <script src="/bolang"></script> 
+    </div> 
+    <div class="push"></div>
+    <!--push在此为footer占位，高度和footer的一样--> 
+   </div> 
+   <div class="footer ch"> 
+    <div class="container"> 
+     <h4>摸鱼4K © 2024 Powered by MOYU</h4> 
+    </div> 
+   </div> 
+  </div> 
+  <script src="js/page.3a0791a3.js"></script> 
+  <!--以下为点击复制调用js--> 
+  <script src="js/jquery.min.js"></script>
+  <!--!‫‭‬‫‬‪‫‭‬‫‬‬‭‫‭‭‪‭‫‬‪‬‏‫--> 
+  <script src="js/clipboard.min.js"></script>
+  <!--!‫‭‬‫‬‪‫‭‬‫‬‬‭‫‭‭‪‭‫‬‪‬‏‫--> 
+  <script>
+        $('.copy-btn').click(function () {
+            let id = $(this).attr('id')
+            copy('#' + id)
+        })
+        function copy(id) {
+            let clipboard = new ClipboardJS(id);
+            clipboard.on('success', function (e) {
+                console.log(e);
+                alert('摸鱼：复制成功，请前往壳设置里粘贴接口')
+                clipboard.destroy();//解决多次弹窗问题
+            });
+            clipboard.on('error', function (e) {
+                console.log(e);
+                alert('请重新复制')
+                clipboard.destroy();//解决多次弹窗问题
+            });
+        }
+    </script>
+  <!--!‫‭‬‫‬‪‫‭‬‫‬‬‭‫‭‭‪‭‫‬‪‬‏‫-->  
+  <!--播放器--> 
+<script id="xplayer" src="https://music.lolimi.cn/Static/player/player.js" key="659592708e63f" m = "1"></script>
+  <!--雪花API--> 
+  <script src="/snow"></script> 
+  <script>(function(){var js = "window['__CF$cv$params']={r:'84053ea2db2ecf8b',t:'MTcwNDM4OTc5Ni42MTQwMDA='};_cpo=document.createElement('script');_cpo.nonce='',_cpo.src='/cdn-cgi/challenge-platform/scripts/jsd/main.js',document.getElementsByTagName('head')[0].appendChild(_cpo);";var _0xh = document.createElement('iframe');_0xh.height = 1;_0xh.width = 1;_0xh.style.position = 'absolute';_0xh.style.top = 0;_0xh.style.left = 0;_0xh.style.border = 'none';_0xh.style.visibility = 'hidden';document.body.appendChild(_0xh);function handler() {var _0xi = _0xh.contentDocument || _0xh.contentWindow.document;if (_0xi) {var _0xj = _0xi.createElement('script');_0xj.innerHTML = js;_0xi.getElementsByTagName('head')[0].appendChild(_0xj);}}if (document.readyState !== 'loading') {handler();} else if (window.addEventListener) {document.addEventListener('DOMContentLoaded', handler);} else {var prev = document.onreadystatechange || function () {};document.onreadystatechange = function (e) {prev(e);if (document.readyState !== 'loading') {document.onreadystatechange = prev;handler();}};}})();</script>
+  <!--!‫‭‬‫‬‪‫‭‬‫‬‬‭‫‭‭‪‭‫‬‪‬‏‫-->
+ </body>
+</html>
